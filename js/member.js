82a26390aa83af4aed49c4888b3ea57025a82436
yu_j <yu_j@vanxsoft.net>
2022-09-02 21:36:27 +0800

    提交名单

--- js/member.js
+++ js/member.js
@@ -1,287 +1,438 @@
 var six_list = [{
-  "six_id": "No.1",
-  "name": "111"
-}]
+    "six_id": "No.1",
+    "name": "顾可欣"
+  },
+  {
+    "six_id": "No.2",
+    "name": "刘姝廷"
+  },
+  {
+    "six_id": "No.3",
+    "name": "魏于倩"
+  },
+  {
+    "six_id": "No.4",
+    "name": "孙语欣"
+  },
+  {
+    "six_id": "No.5",
+    "name": "李同博"
+  },
+  {
+    "six_id": "No.6",
+    "name": "张莉"
+  },
+  {
+    "six_id": "No.7",
+    "name": "陈敏娜"
+  },
+  {
+    "six_id": "No.8",
+    "name": "蒋静怡"
+  },
+  {
+    "six_id": "No.9",
+    "name": "钱慧敏"
+  },
+  {
+    "six_id": "No.10",
+    "name": "江雨纯"
+  },
+  {
+    "six_id": "No.11",
+    "name": "顾翊凡"
+  },
+  {
+    "six_id": "No.12",
+    "name": "潘嘉蕊"
+  },
+  {
+    "six_id": "No.13",
+    "name": "吴靖宇"
+  },
+  {
+    "six_id": "No.14",
+    "name": "陈俊涵"
+  },
+  {
+    "six_id": "No.15",
+    "name": "陈琦"
+  },
+  {
+    "six_id": "No.16",
+    "name": "乔阳"
+  },
+  {
+    "six_id": "No.17",
+    "name": "梁钲宇"
+  },
+  {
+    "six_id": "No.18",
+    "name": "王钲杰"
+  },
+  {
+    "six_id": "No.19",
+    "name": "崔佳怡"
+  },
+  {
+    "six_id": "No.20",
+    "name": "朱静雯"
+  },
+  {
+    "six_id": "No.21",
+    "name": "王一壹"
+  },
+  {
+    "six_id": "No.22",
+    "name": "陈俊豪"
+  },
+  {
+    "six_id": "No.23",
+    "name": "杨永轩"
+  },
+  {
+    "six_id": "No.24",
+    "name": "杨心雨"
+  },
+  {
+    "six_id": "No.25",
+    "name": "陈金豪"
+  },
+  {
+    "six_id": "No.26",
+    "name": "薛讳泽"
+  },
+  {
+    "six_id": "No.27",
+    "name": "支敏慧"
+  },
+  {
+    "six_id": "No.28",
+    "name": "杨雅婷"
+  },
+  {
+    "six_id": "No.29",
+    "name": "单航傲"
+  },
+  {
+    "six_id": "No.30",
+    "name": "王诗涵"
+  },
+  {
+    "six_id": "No.31",
+    "name": "陈品榕"
+  },
+  {
+    "six_id": "No.32",
+    "name": "施苏纾"
+  },
+  {
+    "six_id": "No.33",
+    "name": "王诗棋"
+  },
+  {
+    "six_id": "No.34",
+    "name": "毕思豪"
+  },
+  {
+    "six_id": "No.35",
+    "name": "肖悦"
+  },
+  {
+    "six_id": "No.36",
+    "name": "金筱楠"
+  },
+  {
+    "six_id": "No.37",
+    "name": "陆逸雯"
+  },
+  {
+    "six_id": "No.38",
+    "name": "李轩"
+  },
+  {
+    "six_id": "No.39",
+    "name": "徐思彤"
+  },
+  {
+    "six_id": "No.40",
+    "name": "孙家成"
+  },
+  {
+    "six_id": "No.41",
+    "name": "王博"
+  },
+  {
+    "six_id": "No.42",
+    "name": "徐子霂"
+  },
+  {
+    "six_id": "No.43",
+    "name": "张雨婷"
+  },
+  {
+    "six_id": "No.44",
+    "name": "许鑫宇"
+  },
+  {
+    "six_id": "No.45",
+    "name": "成惠研"
+  },
+  {
+    "six_id": "No.46",
+    "name": "陈子涵"
+  },
+  {
+    "six_id": "No.47",
+    "name": "陈瑞"
+  },
+  {
+    "six_id": "No.48",
+    "name": "李欣玥"
+  },
+  {
+    "six_id": "No.49",
+    "name": "费台昊"
+  },
+  {
+    "six_id": "No.50",
+    "name": "叶政文"
+  },
+  {
+    "six_id": "No.51",
+    "name": "孙悦铭"
+  },
+  {
+    "six_id": "No.52",
+    "name": "于庆哲"
+  },
+
+]
 
 
 var seven_list = [{
     "seven_id": "No.1",
-    "name": "王梦龄"
+    "name": "陈茜"
   },
   {
     "seven_id": "No.2",
-    "name": "邓世杰"
+    "name": "曹宇轩"
   },
   {
     "seven_id": "No.3",
-    "name": "王莹"
+    "name": "钱玉可"
   },
   {
     "seven_id": "No.4",
-    "name": "张雯靖"
+    "name": "张家悦"
   },
   {
     "seven_id": "No.5",
-    "name": "王法鑫"
+    "name": "季兰博"
   },
   {
     "seven_id": "No.6",
-    "name": "程雪"
+    "name": "姜雅雯"
   },
   {
     "seven_id": "No.7",
-    "name": "徐君华"
+    "name": "赵心志"
   },
   {
     "seven_id": "No.8",
-    "name": "季明如"
+    "name": "肖涛"
   },
   {
     "seven_id": "No.9",
-    "name": "耿诗颖"
+    "name": "温潇"
   },
   {
     "seven_id": "No.10",
-    "name": "陈心怡"
+    "name": "张梦瑶"
   },
   {
     "seven_id": "No.11",
-    "name": "张斯杰"
+    "name": "顾语绮"
   },
   {
     "seven_id": "No.12",
-    "name": "李香迎"
+    "name": "李雨馨"
   },
   {
     "seven_id": "No.13",
-    "name": "孟好"
+    "name": "王宇昊"
   },
   {
     "seven_id": "No.14",
-    "name": "陈蕾"
+    "name": "钱寅"
   },
   {
     "seven_id": "No.15",
-    "name": "陈源"
+    "name": "周向芹"
   },
   {
     "seven_id": "No.16",
-    "name": "吕袁"
+    "name": "范诗琪"
   },
   {
     "seven_id": "No.17",
-    "name": "徐美妍"
+    "name": "高瑞"
   },
   {
     "seven_id": "No.18",
-    "name": "朱诗敏"
+    "name": "徐嘉"
   },
   {
     "seven_id": "No.19",
-    "name": "毛嘉祺"
+    "name": "刘梓轩"
   },
   {
     "seven_id": "No.20",
-    "name": "汤翼桐"
+    "name": "李威"
   },
   {
     "seven_id": "No.21",
-    "name": "杨佳瑶"
+    "name": "姜智文"
   },
   {
     "seven_id": "No.22",
-    "name": "缪巧"
+    "name": "王雨琪"
   },
   {
     "seven_id": "No.23",
-    "name": "于登辉"
+    "name": "贾熙媛"
   },
   {
     "seven_id": "No.24",
-    "name": "耿志航"
+    "name": "王子豪"
   },
   {
     "seven_id": "No.25",
-    "name": "郁婕"
+    "name": "闫景昊"
   },
   {
     "seven_id": "No.26",
-    "name": "张梦婷"
+    "name": "葛志豪"
   },
   {
     "seven_id": "No.27",
-    "name": "张国豪"
+    "name": "李佳蓓"
   },
   {
     "seven_id": "No.28",
-    "name": "徐金慧"
+    "name": "祖诗琦"
   },
   {
     "seven_id": "No.29",
-    "name": "吕新举"
+    "name": "姜礼旭"
   },
   {
     "seven_id": "No.30",
-    "name": "易昊晟"
+    "name": "沈煜城"
   },
   {
     "seven_id": "No.31",
-    "name": "张海容"
+    "name": "张婉婷"
   },
   {
     "seven_id": "No.32",
-    "name": "张筱楠"
+    "name": "徐愉涵"
   },
   {
     "seven_id": "No.33",
-    "name": "顾雅件"
+    "name": "左浩然"
   },
   {
     "seven_id": "No.34",
-    "name": "晏思淼"
+    "name": "钱思博"
   },
   {
     "seven_id": "No.35",
-    "name": "祖佳豪"
+    "name": "冯帅家"
   },
   {
     "seven_id": "No.36",
-    "name": "孙浩"
+    "name": "羊爱琳"
   },
   {
     "seven_id": "No.37",
-    "name": "顾妍"
+    "name": "薛皓坤"
   },
   {
     "seven_id": "No.38",
-    "name": "吴堂正"
+    "name": "左诗涵"
   },
   {
     "seven_id": "No.39",
-    "name": "陶永旺"
+    "name": "王若诗"
   },
   {
     "seven_id": "No.40",
-    "name": "龚雨洁"
+    "name": "肖旺泽"
   },
   {
     "seven_id": "No.41",
-    "name": "于庆宝"
+    "name": "祖夕春"
   },
   {
     "seven_id": "No.42",
-    "name": "陈豪杰"
+    "name": "朱家纬"
   },
   {
     "seven_id": "No.43",
-    "name": "王姝"
+    "name": "沙雨萱"
   },
   {
     "seven_id": "No.44",
-    "name": "钱文娟"
+    "name": "张运强"
   },
   {
     "seven_id": "No.45",
-    "name": "张宇豪"
+    "name": "徐涛"
   },
   {
     "seven_id": "No.46",
-    "name": "黄梓桐"
+    "name": "戴皓"
   },
   {
     "seven_id": "No.47",
-    "name": "唐萌萌"
+    "name": "桑伟轩"
   },
   {
     "seven_id": "No.48",
-    "name": "孟星雨"
+    "name": "郁立成"
   },
   {
     "seven_id": "No.49",
-    "name": "石子凡"
+    "name": "钱家豪"
   },
   {
     "seven_id": "No.50",
-    "name": "王兴雄"
+    "name": "肖宇杰"
   },
   {
     "seven_id": "No.51",
-    "name": "叶相竹"
+    "name": "吴子浩"
   },
   {
     "seven_id": "No.52",
-    "name": "茆天豪"
+    "name": "陈叶"
   },
   {
     "seven_id": "No.53",
-    "name": "徐伟杰"
+    "name": "庄俊宇"
   },
   {
     "seven_id": "No.54",
-    "name": "陈泓玮"
+    "name": "郑舒月"
   },
   {
     "seven_id": "No.55",
-    "name": "朱琅慧"
+    "name": "咸文静"
   },
   {
     "seven_id": "No.56",
-    "name": "顾佳怡"
+    "name": "夏正一"
   },
-  {
-    "seven_id": "No.57",
-    "name": "史胜超"
-  },
-  {
-    "seven_id": "No.58",
-    "name": "刘昌荣"
-  },
-  {
-    "seven_id": "No.59",
-    "name": "单明月"
-  },
-  {
-    "seven_id": "No.60",
-    "name": "李浩南"
-  },
-  {
-    "seven_id": "No.61",
-    "name": "田静"
-  },
-  {
-    "seven_id": "No.62",
-    "name": "张国平"
-  },
-  {
-    "seven_id": "No.63",
-    "name": "尹荟茹"
-  },
-  {
-    "seven_id": "No.64",
-    "name": "易成泽"
-  },
-  {
-    "seven_id": "No.65",
-    "name": "黄易锋"
-  },
-  {
-    "seven_id": "No.66",
-    "name": "刘思涵"
-  },
-  {
-    "seven_id": "No.67",
-    "name": "陈宇志"
-  },
-  {
-    "seven_id": "No.68",
-    "name": "杨光"
-  },
-  {
-    "seven_id": "No.69",
-    "name": "张新洲"
-  },
-  {
-    "seven_id": "No.70",
-    "name": "龚胜豪"
-  },
+
 ]
