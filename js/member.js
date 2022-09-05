78600e6fd805095e0e86942171c4ef7d767493c4
人莫予毒 <cavaliers2322115@vip.qq.com>
2022-09-05 11:06:13 +0800

    修改id

--- js/member.js
+++ js/member.js
@@ -1,209 +1,209 @@
 var seven_list = [{
-    "six_id": "No.1",
+    "seven_id": "No.1",
     "name": "顾可欣"
   },
   {
-    "six_id": "No.2",
+    "seven_id": "No.2",
     "name": "刘姝廷"
   },
   {
-    "six_id": "No.3",
+    "seven_id": "No.3",
     "name": "魏于倩"
   },
   {
-    "six_id": "No.4",
+    "seven_id": "No.4",
     "name": "孙语欣"
   },
   {
-    "six_id": "No.5",
+    "seven_id": "No.5",
     "name": "李同博"
   },
   {
-    "six_id": "No.6",
+    "seven_id": "No.6",
     "name": "张莉"
   },
   {
-    "six_id": "No.7",
+    "seven_id": "No.7",
     "name": "陈敏娜"
   },
   {
-    "six_id": "No.8",
+    "seven_id": "No.8",
     "name": "蒋静怡"
   },
   {
-    "six_id": "No.9",
+    "seven_id": "No.9",
     "name": "钱慧敏"
   },
   {
-    "six_id": "No.10",
+    "seven_id": "No.10",
     "name": "江雨纯"
   },
   {
-    "six_id": "No.11",
+    "seven_id": "No.11",
     "name": "顾翊凡"
   },
   {
-    "six_id": "No.12",
+    "seven_id": "No.12",
     "name": "潘嘉蕊"
   },
   {
-    "six_id": "No.13",
+    "seven_id": "No.13",
     "name": "吴靖宇"
   },
   {
-    "six_id": "No.14",
+    "seven_id": "No.14",
     "name": "陈俊涵"
   },
   {
-    "six_id": "No.15",
+    "seven_id": "No.15",
     "name": "陈琦"
   },
   {
-    "six_id": "No.16",
+    "seven_id": "No.16",
     "name": "乔阳"
   },
   {
-    "six_id": "No.17",
+    "seven_id": "No.17",
     "name": "梁钲宇"
   },
   {
-    "six_id": "No.18",
+    "seven_id": "No.18",
     "name": "王钲杰"
   },
   {
-    "six_id": "No.19",
+    "seven_id": "No.19",
     "name": "崔佳怡"
   },
   {
-    "six_id": "No.20",
+    "seven_id": "No.20",
     "name": "朱静雯"
   },
   {
-    "six_id": "No.21",
+    "seven_id": "No.21",
     "name": "王一壹"
   },
   {
-    "six_id": "No.22",
+    "seven_id": "No.22",
     "name": "陈俊豪"
   },
   {
-    "six_id": "No.23",
+    "seven_id": "No.23",
     "name": "杨永轩"
   },
   {
-    "six_id": "No.24",
+    "seven_id": "No.24",
     "name": "杨心雨"
   },
   {
-    "six_id": "No.25",
+    "seven_id": "No.25",
     "name": "陈金豪"
   },
   {
-    "six_id": "No.26",
+    "seven_id": "No.26",
     "name": "薛讳泽"
   },
   {
-    "six_id": "No.27",
+    "seven_id": "No.27",
     "name": "支敏慧"
   },
   {
-    "six_id": "No.28",
+    "seven_id": "No.28",
     "name": "杨雅婷"
   },
   {
-    "six_id": "No.29",
+    "seven_id": "No.29",
     "name": "单航傲"
   },
   {
-    "six_id": "No.30",
+    "seven_id": "No.30",
     "name": "王诗涵"
   },
   {
-    "six_id": "No.31",
+    "seven_id": "No.31",
     "name": "刘泽旻"
   },
   {
-    "six_id": "No.32",
+    "seven_id": "No.32",
     "name": "施苏纾"
   },
   {
-    "six_id": "No.33",
+    "seven_id": "No.33",
     "name": "王诗棋"
   },
   {
-    "six_id": "No.34",
+    "seven_id": "No.34",
     "name": "毕思豪"
   },
   {
-    "six_id": "No.35",
+    "seven_id": "No.35",
     "name": "肖悦"
   },
   {
-    "six_id": "No.36",
+    "seven_id": "No.36",
     "name": "金筱楠"
   },
   {
-    "six_id": "No.37",
+    "seven_id": "No.37",
     "name": "陆逸雯"
   },
   {
-    "six_id": "No.38",
+    "seven_id": "No.38",
     "name": "李轩"
   },
   {
-    "six_id": "No.39",
+    "seven_id": "No.39",
     "name": "徐思彤"
   },
   {
-    "six_id": "No.40",
+    "seven_id": "No.40",
     "name": "孙家成"
   },
   {
-    "six_id": "No.41",
+    "seven_id": "No.41",
     "name": "王博"
   },
   {
-    "six_id": "No.42",
+    "seven_id": "No.42",
     "name": "徐子霂"
   },
   {
-    "six_id": "No.43",
+    "seven_id": "No.43",
     "name": "张雨婷"
   },
   {
-    "six_id": "No.44",
+    "seven_id": "No.44",
     "name": "许鑫宇"
   },
   {
-    "six_id": "No.45",
+    "seven_id": "No.45",
     "name": "成惠研"
   },
   {
-    "six_id": "No.46",
+    "seven_id": "No.46",
     "name": "陈子涵"
   },
   {
-    "six_id": "No.47",
+    "seven_id": "No.47",
     "name": "陈瑞"
   },
   {
-    "six_id": "No.48",
+    "seven_id": "No.48",
     "name": "李欣玥"
   },
   {
-    "six_id": "No.49",
+    "seven_id": "No.49",
     "name": "费台昊"
   },
   {
-    "six_id": "No.50",
+    "seven_id": "No.50",
     "name": "叶政文"
   },
   {
-    "six_id": "No.51",
+    "seven_id": "No.51",
     "name": "孙悦铭"
   },
   {
-    "six_id": "No.52",
+    "seven_id": "No.52",
     "name": "于庆哲"
   }
 ]
